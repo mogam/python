900b3a10a9af8c76320402e3806fa9d22666098f
unknown <pushpakthakur116@gmail.com>
2022-06-27 22:46:14 +0530

    joidhuiweb gu u gd

--- conditions.py
+++ conditions.py
@@ -1,5 +1,7 @@
-n = input("Number: ")
+n = int(input("Number: "))
 if n > 0:
     print("n is positive")
-
-        print("n is positive")
+elif n < 0:
+    print("n is negative")
+else:
+    print("n is zero")
