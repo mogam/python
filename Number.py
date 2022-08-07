069a5ea4fe22767e3d56000b0a71b84612522012
unknown <pushpakthakur116@gmail.com>
2022-08-07 20:34:48 +0530

    First release of Hello World!

---
+++ Number.py
@@ -0,0 +1,9 @@
+n = int(input("Number"))
+
+if n > 0:
+    print("N is positive")
+elif n < 0:
+    print("N is Negative")
+else:
+    print("N is Zero")
+
---
+++ sets.py
@@ -0,0 +1,12 @@
+s = set()
+
+s.add("Pushpak")
+s.add("Thakur")
+s.add("anjli")
+s.add("Raj")
+s.add("Raj")
+
+s.remove("Raj")
+print(f"The Set has {len(s)} element")
+
+
